61228c461fceeee454e9622572e0b09d8713c6d0
unknown <zeiadsamir33@gmail.com>
2021-11-12 01:52:49 +0200

    First Submit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# LandingPage
-This project from Udacity was testing JavaScript skills and manipulation with DOM.
+# Title
+
+Landing Page
+
+## About The Project
+
+In this project, we're going to build a dynamic landing page which is responsive and totally dynamic with JavaScript DOM.
+
+### Built with
+
+- HTML
+- CSS
+- JavaScript
+
+#### Usage
+
+
+
---
+++ js/app.js
@@ -0,0 +1,81 @@
+
+// store all sections in on array
+let sections = document.querySelectorAll('section');
+
+// Create Dynamic navBar
+sections.forEach(function(section){
+
+    let li = document.createElement("li");
+    let link = document.createElement("a");
+    let anchorText = document.createTextNode(section.dataset.nav);
+
+
+    link.setAttribute("class","menu__link");
+    //link.setAttribute('href',`#${section.id}`);
+    link.appendChild(anchorText);
+    li.appendChild(link);
+    document.getElementById('navbar__list').appendChild(li);
+
+});
+
+// add active while scrolling to specific section.
+
+window.addEventListener('scroll', () => {
+
+sections.forEach ( section => {
+    let high  = window.pageYOffset
+    if(high >= 400 && high <= 1000){
+        sections[0].setAttribute('class','your-active-class');
+        sections[1].removeAttribute('class','your-active-class');
+        sections[2].removeAttribute('class','your-active-class');
+        sections[3].removeAttribute('class','your-active-class');
+    }else if(high >= 1000 && high <= 1600){
+        sections[0].removeAttribute('class','your-active-class');
+        sections[1].setAttribute('class','your-active-class');
+        sections[2].removeAttribute('class','your-active-class');
+        sections[3].removeAttribute('class','your-active-class');
+
+    }
+    else if(high >= 1600 && high <= 2200){
+        sections[0].removeAttribute('class','your-active-class');
+        sections[1].removeAttribute('class','your-active-class');
+        sections[2].setAttribute('class','your-active-class');
+        sections[3].removeAttribute('class','your-active-class');
+    }
+    else if(high >= 2200 && high <= 2800){
+        sections[2].removeAttribute('class','your-active-class');
+        sections[3].setAttribute('class','your-active-class');
+    }
+});
+});
+
+// Scroll To Section One
+let anchorOne = document.getElementsByClassName('menu__link')[0];
+let secOne = document.getElementById('section1');
+
+anchorOne.addEventListener('click', function() {
+    secOne.scrollIntoView({behavior:'smooth'});
+    //anchorOne.preventDefault();
+})
+
+// Scroll To Section Two
+let anchorTwo = document.getElementsByClassName('menu__link')[1];
+let secTwo = document.getElementById('section2');
+
+anchorTwo.addEventListener('click', function() {
+    secTwo.scrollIntoView({behavior:'smooth'});
+})
+// Scroll To Section Three
+let anchorThree = document.getElementsByClassName('menu__link')[2];
+let secThree = document.getElementById('section3');
+
+anchorThree.addEventListener('click', function() {
+    secThree.scrollIntoView({behavior:'smooth'});
+})
+// Scroll To Section Four
+let anchorFour = document.getElementsByClassName('menu__link')[3];
+let secFour = document.getElementById('section4');
+
+anchorFour.addEventListener('click', function() {
+    secFour.scrollIntoView({behavior:'smooth'});
+})
