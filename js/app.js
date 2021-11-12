f64527d19c30b6a23b5efc7235a3efd09d1015c6
unknown <zeiadsamir33@gmail.com>
2021-11-12 02:34:53 +0200

    Updated

--- js/app.js
+++ js/app.js
@@ -79,3 +79,21 @@ let secFour = document.getElementById('section4');
 anchorFour.addEventListener('click', function() {
     secFour.scrollIntoView({behavior:'smooth'});
 })
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
